adcb9a369bab992b0bb2b3eba948531c763de459
CricGuru <35167340+CricGuru@users.noreply.github.com>
2021-03-02 13:20:52 +0000

    added some test cases for rrtstar tester

--- RRTStarTester.m
+++ RRTStarTester.m
@@ -37,7 +37,7 @@ assert(isequal(occupancy,zeros(sz)))
 % Example 2 Preparation
 
 start = [-410,210]
-goal = [150,-150]
+goal = [150,150]
 
 % Get route vector
 route = computeRRTStarRoute(start, goal)
@@ -82,13 +82,48 @@ assert(isequal(occupancy,zeros(sz)))
 
 
 
+% Example 4 Preparation
 
-% 
+start = [-250,-250]
+goal = [500,-300]
+
+% Get route vector
+route = computeRRTStarRoute(start, goal)
+
+% Test 1: Start is correct
+
+assert(isequal(route(1,1:2),start), "Start is wrong")
+
+% Test 2: Goal is correct
+
+assert(isequal(int32(route(end,1:2)),goal), "Goal is wrong")
+
+% Test 3: Check if points along the route are in buildings
+
+occupancy = getOccupancy(omap,route);
+sz = size(occupancy);
+assert(isequal(occupancy,zeros(sz)))
 
 
 
+% Example 5 Preparation
 
+start = [-250,-250]
+goal = [500,-300]
 
+% Get route vector
+route = computeRRTStarRoute(start, goal)
 
+% Test 1: Start is correct
 
+assert(isequal(route(1,1:2),start), "Start is wrong")
 
+% Test 2: Goal is correct
+
+assert(isequal(int32(route(end,1:2)),goal), "Goal is wrong")
+
+% Test 3: Check if points along the route are in buildings
+
+occupancy = getOccupancy(omap,route);
+sz = size(occupancy);
+assert(isequal(occupancy,zeros(sz)))
