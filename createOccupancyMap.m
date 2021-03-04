ff00d8f2c72ad35465e7672005eaf17d57317621
Thomas Patterson <thomasbpatterson@btinternet.com>
2021-03-04 12:04:13 +0000

    add walls to omap so we dont fly off the map

--- createOccupancyMap.m
+++ createOccupancyMap.m
@@ -43,16 +43,58 @@ for i=1:length(vertices)
     if ~isempty(succX)
         setOccupancy(omap, transpose([succX; succY]), 1);
     end
-    
 end
 
 inflate(omap, 2);
 
-topLeft = [-560, -310];
-botRight = [520, 310];
+topLeft = [40, 290];
+botRight = [1120, 910];
 
 xPoints = [];
 yPoints = [];
 
+for x=40:1120
+   xPoints(8*(x-39)) = x;
+   xPoints(8*(x-39)-3) = x;
+   xPoints(8*(x-39)-2) = x;
+   xPoints(8*(x-39)-1) = x;
+   xPoints(8*(x-39)-7) = x;
+   xPoints(8*(x-39)-6) = x;
+   xPoints(8*(x-39)-5) = x;
+   xPoints(8*(x-39)-4) = x;
+   yPoints(8*(x-39)) = 289;
+   yPoints(8*(x-39)-1) = 290;
+   yPoints(8*(x-39)-2) = 910;
+   yPoints(8*(x-39)-3) = 911;
+   yPoints(8*(x-39)-7) = 288;
+   yPoints(8*(x-39)-6) = 287;
+   yPoints(8*(x-39)-5) = 912;
+   yPoints(8*(x-39)-4) = 913;
+end
+setOccupancy(omap, transpose([xPoints; yPoints]), 1);
 
-save("omap.mat", "omap");
+xPoints = [];
+yPoints = [];
+
+for y=290:910
+   xPoints(8*(y-289)) = 40;
+   xPoints(8*(y-289)-3) = 39;
+   xPoints(8*(y-289)-2) = 38;
+   xPoints(8*(y-289)-1) = 37;
+   xPoints(8*(y-289)-7) = 1120;
+   xPoints(8*(y-289)-6) = 1121;
+   xPoints(8*(y-289)-5) = 1122;
+   xPoints(8*(y-289)-4) = 1123;
+   yPoints(8*(y-289)) = y;
+   yPoints(8*(y-289)-1) = y;
+   yPoints(8*(y-289)-2) = y;
+   yPoints(8*(y-289)-3) = y;
+   yPoints(8*(y-289)-7) = y;
+   yPoints(8*(y-289)-6) = y;
+   yPoints(8*(y-289)-5) = y;
+   yPoints(8*(y-289)-4) = y;
+end
+
+setOccupancy(omap, transpose([xPoints; yPoints]), 1);
+
+save("work/omap.mat", "omap");
