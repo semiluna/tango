96a2621b959f3a0783ea1f395cea9351ab78c40c
Antonia Boca <bocaantonia@gmail.com>
2021-03-05 20:33:24 +0000

    modify schedule wrapper

--- scheduler_wrapper.m
+++ scheduler_wrapper.m
@@ -1,6 +1,8 @@
 % add missions from user interface 
 
-function scheduler_wrapper() 
+function scheduler_wrapper()
+    cachedDistance = memoize(@compute_eucldian_distance);
+    
     if ~isempty(hApp.newMission)
 
         newMission = hApp.newMission;
@@ -28,6 +30,7 @@ function scheduler_wrapper()
             indexStart = coord_map.values(start);
         else
             indexStart = nodes + l;
+            nodes = nodes + 1;
             coord_map(start) = indexStart;
         end
         
@@ -35,44 +38,30 @@ function scheduler_wrapper()
             indexStart = coord_map.values(goal);
         else
             indexGoal = nodes + 2;
+            nodes = nodes + 1;
             coord_map(goal) = indexGoal;
         end
         
-        nodes = nodes + 2;
         
         % add distances to the adjacency matrix
         
-        % route_cache([indexStart indexGoal]) = route; %list of intermediate points
-        % route_cache([indexGoal indexStart]) = route; 
-        % distance_cache(indexStart, indexGoal) = compute_euclidian_distance(route); %total distance of route
-        % distance_cache(indexGoal, indexStart)  = distance_cache(indexStart, indexGoal);
+        distance_cache(indexStart, indexGoal) = cachedDistance(route);
+        distance_cache(indexGoal, indexStart) = cachedDistance(route);
         
         % remember GroundControl routes
         
-        % groundControl_cache(indexStart) = gcDistancePickup;
-        % groundControl_cache(indexGoal) = gcDistanceDelivery;
-        
-        % if gcDistance(indexStart) == 0
-        %    gcDistance(indexStart) = compute_euclidian_distance(gcDistancePickup);
-        % end
-        
-        % if gcDistance(indexGoal) == 0
-        %    gcDistance(indexGoal) = compute_euclidian_distance(gcDistanceDelivery);
-        % end
+        gcDistance(indexStart) = cachedDistance(gcDistancePickup);
+        gcDistance(indexGoal) = cachedDistance(gcDistanceDelivery);
         
         % compute distances from point "goal" to every other possible
         % "pickup" point
         for index = left : (right - 1)
             mission = mission_queue(index); % pair of indices (indexStart, indexGoal)
-            indexNextStart = mission(2);
+            indexNextStart = mission(2); % next pickup point
             
-            if ~route_cache.isKey([indexGoal indexNextStart])
-                nextRoute = computeRRTStarRoute(indexGoal, indexNextStart);
-                % route_cache([indexGoal indexNextStart]) = nextRoute;
-                % route_cache([indexNextStart indexGoal]) = nextRoute;
-                % distance_cache(indexGoal, indexNextStart) = compute_euclidian_distance(route);
-                % distance_cache(indexNextStart, indexGoal) = distance_cache(indexGoal, indexNextStart);
-            end       
+            nextRoute = computeRRTStarRoute(indexGoal, indexNextStart);
+            distance_cache(indexGoal, indexNextStart) = cachedDistance(nextRoute);
+            distance_cache(indexNextStart, indexGoal) = cachedDistance(nextRoute);
         end
         
         
@@ -81,15 +70,13 @@ function scheduler_wrapper()
         mission_queue(right) = [indexStart indexGoal];
         right = right + 1;
         
-        % compute a new set of missions
-        mission_list = mission_queue(left:(right-1));
-        
-        solution = max_time_matching(right - left, nodes, 100, mission_list, distance_cache, gcDistance);
+        solution = max_time_matching(right - left, nodes, 100, mission_queue, distance_cache, gcDistance);
         
         % what do to with the solution?
         
         disp(solution);
-    
+        
+        hApp.newMission = [];
     end
     
     
