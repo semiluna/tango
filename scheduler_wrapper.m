0fb76ffbff2056daea48278a48ce82e2ecb435f2
nikgeopro <nikola.georgiev2000@hotmail.com>
2021-03-06 23:00:53 +0100

    Work on app UI and wrapper. (bringing things together)

--- scheduler_wrapper.m
+++ scheduler_wrapper.m
@@ -1,83 +1,73 @@
 % add missions from user interface 
 
-function scheduler_wrapper()
+function schedule = scheduler_wrapper(newMission, coord_map, distance_cache, gcDistance, mission_queue, left, right)
     cachedDistance = memoize(@compute_eucldian_distance);
     
-    if ~isempty(hApp.newMission)
+    start = newMission(1,:);
+    goal = newMission(2,:);
 
-        newMission = hApp.newMission;
+    % start and goal are longitude and latitude, groundControl is the
+    % middle of the map
 
-        start = newMission(1);
-        goal = newMission(2);
-        
-        hApp.newMission = [];
-        
-        % start and goal are longitude and latitude, groundControl is the
-        % middle of the map
-    
-        route = computeRRTStarRoute(start, goal);
-        
-        groundControl = [0 0];
-        
-        gcDistancePickup = computeRRTStarRoute(groundControl, start);
-        gcDistanceDelivery = computeRRTStartRoute(goal, groundControl);
-        
-        nodes = coord_map.size();
-        
-        % now indexStart is the index of point start and indexGoal is the index of point goal
-        
-        if coord_map.isKey(start)
-            indexStart = coord_map.values(start);
-        else
-            indexStart = nodes + l;
-            nodes = nodes + 1;
-            coord_map(start) = indexStart;
-        end
-        
-        if coord_map.isKey(goal)
-            indexStart = coord_map.values(goal);
-        else
-            indexGoal = nodes + 2;
-            nodes = nodes + 1;
-            coord_map(goal) = indexGoal;
-        end
-        
-        
-        % add distances to the adjacency matrix
-        
-        distance_cache(indexStart, indexGoal) = cachedDistance(route);
-        distance_cache(indexGoal, indexStart) = cachedDistance(route);
-        
-        % remember GroundControl routes
-        
-        gcDistance(indexStart) = cachedDistance(gcDistancePickup);
-        gcDistance(indexGoal) = cachedDistance(gcDistanceDelivery);
-        
-        % compute distances from point "goal" to every other possible
-        % "pickup" point
-        for index = left : (right - 1)
-            mission = mission_queue(index); % pair of indices (indexStart, indexGoal)
-            indexNextStart = mission(2); % next pickup point
-            
-            nextRoute = computeRRTStarRoute(indexGoal, indexNextStart);
-            distance_cache(indexGoal, indexNextStart) = cachedDistance(nextRoute);
-            distance_cache(indexNextStart, indexGoal) = cachedDistance(nextRoute);
-        end
-        
-        
-       % add read mission to the mission queue
-       
-        mission_queue(right) = [indexStart indexGoal];
-        right = right + 1;
-        
-        solution = max_time_matching(right - left, nodes, 100, mission_queue, distance_cache, gcDistance);
-        
-        % what do to with the solution?
-        
-        disp(solution);
-        
-        hApp.newMission = [];
+    route = computeRRTStarRoute(start, goal);
+
+    groundControl = [0 0];
+
+    gcDistancePickup = computeRRTStarRoute(groundControl, start);
+    gcDistanceDelivery = computeRRTStarRoute(goal, groundControl);
+
+    nodes = coord_map.size();
+
+    % now indexStart is the index of point start and indexGoal is the index of point goal
+
+    if coord_map.isKey(start)
+        indexStart = coord_map.values(start);
+    else
+        indexStart = nodes + l;
+        nodes = nodes + 1;
+        coord_map(start) = indexStart;
     end
+
+    if coord_map.isKey(goal)
+        indexStart = coord_map.values(goal);
+    else
+        indexGoal = nodes + 2;
+        nodes = nodes + 1;
+        coord_map(goal) = indexGoal;
+    end
+
+
+    % add distances to the adjacency matrix
+
+    distance_cache(indexStart, indexGoal) = cachedDistance(route);
+    distance_cache(indexGoal, indexStart) = cachedDistance(route);
+
+    % remember GroundControl routes
+
+    gcDistance(indexStart) = cachedDistance(gcDistancePickup);
+    gcDistance(indexGoal) = cachedDistance(gcDistanceDelivery);
+
+    % compute distances from point "goal" to every other possible
+    % "pickup" point
+    for index = left : (right - 1)
+        mission = mission_queue(index); % pair of indices (indexStart, indexGoal)
+        indexNextStart = mission(2); % next pickup point
+
+        nextRoute = computeRRTStarRoute(indexGoal, indexNextStart);
+        distance_cache(indexGoal, indexNextStart) = cachedDistance(nextRoute);
+        distance_cache(indexNextStart, indexGoal) = cachedDistance(nextRoute);
+    end
+
+
+   % add read mission to the mission queue
+
+    mission_queue(right) = [indexStart indexGoal];
+    right = right + 1;
+
+    solution = max_time_matching(right - left, nodes, 100, mission_queue, distance_cache, gcDistance);
     
+    % what do to with the solution?
+    
+    schedule = solution;
     
 end
